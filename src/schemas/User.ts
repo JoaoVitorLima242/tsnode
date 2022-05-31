d42d89840b90418f4b69d4c0d20ab4f7a0344fd5
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-31 16:23:45 -0300

    add types to schemas

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,6 +1,14 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
-const UserSchema = new Schema({
+interface UserInterface extends Document {
+    email?: string
+    firstName?: string
+    lastName?: string
+    fullName(): string
+}
+
+
+const UserSchema = new Schema<UserInterface>({
     email: String,
     firstName: String,
     lastName: String,
@@ -8,4 +16,8 @@ const UserSchema = new Schema({
     timestamps: true
 })
 
-export default model("User", UserSchema);
+UserSchema.methods.fullName = function(): string {
+    return this.firstName + ' ' +this.lastName
+}
+
+export default model<UserInterface>("User", UserSchema);

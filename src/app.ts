d3adf1be55e9f1f63bcfc810419c1b8bb91bd37f
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-31 15:14:39 -0300

    create app with types

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { applyFixes } from 'eslint/lib/linter/source-code-fixer';
+
+class App {
+    public express: express.Application;
+
+    public constructor () {
+        this.express = express()
+    }
+
+    private middlawares () :void{
+
+        this.express.use(express.json())
+        this.express.use(cors())
+    }
+
+    private database () :void {
+        mongoose.connect('mongodb://localhost27017/tsnode', {
+            useNewUrlParser: true
+        })
+    }
+
+    private routes () : void {
+        this.express.get('/', (req, res) => {
+            return res.send('Hello World');
+        })
+    }
+}
